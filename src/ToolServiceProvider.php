17c260ccdb7029e3888940d664bc03cbff37dd8a
Mr.VACO <brute1904@gmail.com>
2023-06-28 16:27:48 +0500

    Removed unnecessary

--- src/ToolServiceProvider.php
+++ src/ToolServiceProvider.php
@@ -2,12 +2,9 @@
 
 namespace MrVaco\NovaGallery;
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Nova\Events\ServingNova;
-use Laravel\Nova\Http\Middleware\Authenticate;
 use Laravel\Nova\Nova;
-use MrVaco\NovaGallery\Http\Middleware\Authorize;
 
 class ToolServiceProvider extends ServiceProvider
 {
@@ -18,11 +15,6 @@ class ToolServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->app->booted(function()
-        {
-            $this->routes();
-        });
-        
         Nova::serving(function(ServingNova $event)
         {
             Nova::tools([
@@ -32,32 +24,9 @@ class ToolServiceProvider extends ServiceProvider
     }
     
     /**
-     * Register the tool's routes.
-     *
-     * @return void
-     */
-    protected function routes(): void
-    {
-        if ($this->app->routesAreCached())
-        {
-            return;
-        }
-        
-        Nova::router(['nova', Authenticate::class, Authorize::class], 'nova-gallery')
-            ->group(__DIR__ . '/../routes/inertia.php');
-        
-        Route::middleware(['nova', Authorize::class])
-            ->prefix('nova-vendor/nova-gallery')
-            ->group(__DIR__ . '/../routes/api.php');
-    }
-    
-    /**
      * Register any application services.
      *
      * @return void
      */
-    public function register(): void
-    {
-    
-    }
+    public function register(): void {}
 }
